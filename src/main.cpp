f7fb30155f1314e4641feca6fbc2a9f9952fd553
FireMario211 <17692105+FireMario211@users.noreply.github.com>
2024-04-21 21:53:42 -0400

    geode ios is real

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,15 @@
 #include <Geode/modify/PlayLayer.hpp>
 using namespace geode::prelude;
 
+#ifdef GEODE_IS_IOS
+bool dontChangeFont = true;
+#else
+bool dontChangeFont = false;
+#endif
+
 class $modify(PlayLayer) {
     CCLabelBMFont* percentLabel;
+    CCLabelBMFont* goldenLabel;
     float oldScale;
     float oldAnchorPoint = -1.0F;
     void updateProgressbar() {
@@ -17,86 +24,87 @@ class $modify(PlayLayer) {
                     auto labelEnding = labelTest->getString() + (strlen(labelTest->getString()) - 1);
                     if (strcmp(labelEnding, "%") == 0) {
                         m_fields->percentLabel = labelTest;
+                        if (dontChangeFont) {
+                            m_fields->goldenLabel = CCLabelBMFont::create("0%", "goldFont.fnt");
+                            m_fields->goldenLabel->setScale(labelTest->getScale() + 0.15F);
+                            m_fields->goldenLabel->setVisible(false);
+                            m_fields->goldenLabel->setAnchorPoint({0.5,m_fields->percentLabel->getAnchorPoint().y-0.05F});
+                            m_fields->goldenLabel->setZOrder(16);
+                            m_fields->percentLabel->getParent()->addChild(m_fields->goldenLabel);
+                        }
                         m_fields->oldScale = labelTest->getScale();
                         break;
                     }
                 }
             }
         } else {
+            if (m_fields->goldenLabel != nullptr) {
+                m_fields->goldenLabel->setPosition(m_fields->percentLabel->getPosition());
+                m_fields->goldenLabel->setString(m_fields->percentLabel->getString());
+                m_fields->goldenLabel->setAnchorPoint({m_fields->percentLabel->getAnchorPoint().x, m_fields->goldenLabel->getAnchorPoint().y});
+            }
             auto showInPractice = Mod::get()->getSettingValue<bool>("practice-mode");
+            auto showInTestMode = Mod::get()->getSettingValue<bool>("test-mode");
             if (!m_level->isPlatformer()) {
                 float anchorPointX = m_fields->percentLabel->getAnchorPoint().x;
-                if (!showInPractice && m_isPracticeMode) {
-                    m_fields->percentLabel->setFntFile("bigFont.fnt");
-                    m_fields->percentLabel->setScale(m_fields->oldScale);
+                #ifndef GEODE_IS_IOS
+                if ((!showInPractice && m_isPracticeMode) || (!showInTestMode && m_isTestMode)) {
+                    if (dontChangeFont) {
+                        m_fields->goldenLabel->setVisible(false);
+                        m_fields->percentLabel->setVisible(true);
+                    } else {
+                        #ifndef GEODE_IS_IOS
+                        m_fields->percentLabel->setFntFile("bigFont.fnt");
+                        m_fields->percentLabel->setScale(m_fields->oldScale);
+                        #endif
+                    }
                     return;
                 }
-                /* 
-                   pfVar2 = (float *)(**(code **)(**(int **)&(this->GJBaseGameLayer_data).field_0x738 + 100))
-                              (local_14);
-  fVar6 = *(float *)&this->field_0x2aa0;
-  fVar5 = *pfVar2;
-  iVar4 = *(int *)(*(int *)&(this->GJBaseGameLayer_data).field_0x4a0 + 0x414);
-  if (0 < iVar4) {
-    fVar5 = (float)*(int *)&(this->GJBaseGameLayer_data).field_0x200;
-    fVar6 = (float)iVar4;
-  }
-  local_30 = (fVar5 / fVar6) * 100.0;
-  if (local_30 < 0.0) {
-LAB_006e5591:
-    local_30 = 0.0;
-LAB_006e5599:
-    fVar6 = 0.01;
-    if (0.01 <= local_30) goto LAB_006e54f1;
-  }
-  else {
-    if (local_30 == 100.0) {
-      if (local_30 < 0.0) goto LAB_006e5591;
-      goto LAB_006e5599;
-    }
-    local_30 = 100.0;
-LAB_006e54f1:
-    fVar6 = local_30;
-  }
-  fVar5 = *(float *)&this[1].field_0xd4;
-  if ((fVar6 / 100.0) * fVar5 <= fVar5) {
-    fVar6 = 0.01;
-    if (0.01 <= local_30) {
-      fVar6 = local_30;
-    }
-    fVar5 = (fVar6 / 100.0) * fVar5;
-  }
-  iVar4 = **(int **)&this[1].field_0xd0;
-                 */
-                //m_unknownD00
-                //m_unknownDB4
-                //float percent = (m_player1->getPositionX() / m_endPortal->getSpawnPos().x) * 100;
+                #endif
+#ifdef GEODE_IS_IOS
+                float percent = static_cast<int>(PlayLayer::getCurrentPercent());
+#else
                 float percent = static_cast<float>(PlayLayer::getCurrentPercentInt());
+#endif
                 if (m_fields->oldAnchorPoint == -1.0F) {
                     m_fields->oldAnchorPoint = m_fields->percentLabel->getAnchorPoint().y;
                 }
-                
-                //std::cout << "end = " << this->m_endPortal->getSpawnPos().x << " , player = " << m_player1->getPositionX() << ", normalPercent = " << m_level->m_normalPercent << ", divided = " << percent << std::endl;
                 if (percent > (m_level->m_normalPercent)) {
-                    m_fields->percentLabel->setFntFile("goldFont.fnt");
-                    m_fields->percentLabel->setScale(m_fields->oldScale + 0.15F);
-                    m_fields->percentLabel->setAnchorPoint({anchorPointX,m_fields->oldAnchorPoint-0.05F}); // since robert wont allow us to change the Y pos, we will just have to change the anchor point!
+                    if (!dontChangeFont) {
+                        #ifndef GEODE_IS_IOS
+                        m_fields->percentLabel->setFntFile("goldFont.fnt");
+                        m_fields->percentLabel->setScale(m_fields->oldScale + 0.15F);
+                        m_fields->percentLabel->setAnchorPoint({anchorPointX,m_fields->oldAnchorPoint-0.05F}); // since robert wont allow us to change the Y pos, we will just have to change the anchor point!
+                        #endif
+                    } else {
+                        m_fields->goldenLabel->setVisible(true);
+                        m_fields->percentLabel->setScale(0);
+                    }
                 } else {
-                    m_fields->percentLabel->setFntFile("bigFont.fnt");
-                    m_fields->percentLabel->setScale(m_fields->oldScale);
-                    m_fields->percentLabel->setAnchorPoint({anchorPointX,m_fields->oldAnchorPoint});
+                    if (dontChangeFont) {
+                        m_fields->goldenLabel->setVisible(false);
+                        m_fields->percentLabel->setScale(m_fields->oldScale);
+                    } else {
+                        #ifndef GEODE_IS_IOS
+                        m_fields->percentLabel->setFntFile("bigFont.fnt");
+                        m_fields->percentLabel->setScale(m_fields->oldScale);
+                        m_fields->percentLabel->setAnchorPoint({anchorPointX,m_fields->oldAnchorPoint});
+                        #endif
+                    }
                 }
             }
         }
     }
+#ifndef GEODE_IS_IOS
     void updateTimeLabel(int p0, int p1, bool p2) {
         PlayLayer::updateTimeLabel(p0,p1,p2);
         if (m_fields->percentLabel == nullptr) return; // prevent crashes
         float actualTime = p0 + (static_cast<float>(p1) / 100.F);
         auto showInPractice = Mod::get()->getSettingValue<bool>("practice-mode");
+        auto showInTestMode = Mod::get()->getSettingValue<bool>("test-mode");
         if (m_level->isPlatformer() && Mod::get()->getSettingValue<bool>("platformer-mode")) {
             float anchorPointX = m_fields->percentLabel->getAnchorPoint().x;
-            if (!showInPractice && m_isPracticeMode) {
+            if ((!showInPractice && m_isPracticeMode) || (!showInTestMode && m_isTestMode)) {
                 m_fields->percentLabel->setFntFile("bigFont.fnt");
                 m_fields->percentLabel->setScale(m_fields->oldScale);
                 return;
@@ -116,4 +124,5 @@ LAB_006e54f1:
             }
         }
     }
+#endif
 };
